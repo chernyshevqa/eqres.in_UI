70f15659ccbe4947dc625494e6d57cc316c47d34
Жеконя <chernyshevqa@gmail.com>
2023-12-25 09:49:14 +0300

    изменения и добавления по проекту

--- pajes/main_page.py
+++ pajes/main_page.py
@@ -1,9 +1,6 @@
-import time
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver import Keys
 from base.base_class import Base
+from examples_responce.example_responce import *
 
 """Главная страница сайта"""
 
@@ -11,15 +8,11 @@ from base.base_class import Base
 class TestMainPage(Base):
     url = 'https://reqres.in'
 
-    def __init__(self, driver):
-        super().__init__(driver)
-        self.driver = driver
-
     """Локаторы"""
     get_list_user = (By.XPATH, "//li[@class='active']")
     get_single_user = (By.XPATH, "//li[@data-id='users-single']")
     get_single_user_not_found = (By.XPATH, "//li[@data-id='users-single-not-found']")
-    get_list_resourse = (By.XPATH, "//li[@data-id='unknown']")
+    get_list_resource = (By.XPATH, "//li[@data-id='unknown']")
     get_single_resourse = (By.XPATH, "//li[@data-id='unknown-single']")
     get_single_resourse_not_found = (By.XPATH, "//li[@data-id='unknown-single-not-found']")
     post_create = (By.XPATH, "//li[@data-id='post']")
@@ -31,14 +24,96 @@ class TestMainPage(Base):
     post_login_successful = (By.XPATH, "//li[@data-id='login-successful']")
     post_login_unsuccessful = (By.XPATH, "//li[@data-id='login-unsuccessful']")
     get_delayed_responce = (By.XPATH, "//li[@data-id='delay']")
+    status_code = (By.XPATH, "//span[@class='response-code']")
     result_field = (By.XPATH, "//pre[@data-key='output-response']")
+    path = (By.XPATH, "//span[@class='url']")
+    other_page_with_json = (By.XPATH, "//pre[@style='word-wrap: break-word; white-space: pre-wrap;']")
 
     """Методы"""
 
     def test_get_list_user(self):
-        self.click(self.get_list_resourse)
-        time.sleep(3)
+        self.driver.get(self.url)
+        self.click(self.get_list_user)
+        self.get_json(self.result_field, json_get_list_user)
+        self.get_text(self.status_code, status_codes["200"])
 
+    def test_get_single_user(self):
+        self.driver.get(self.url)
+        self.click(self.get_single_user)
+        self.get_text(self.status_code, status_codes["200"])
 
+    def test_get_single_user_not_found(self):
+        self.driver.get(self.url)
+        self.click(self.get_single_user_not_found)
+        self.get_text(self.status_code, status_codes["404"]) # тут тест падает потому, что отображает статус код 200 хотя на странице отображается 404
 
+    def test_get_list_resource(self):
+        self.driver.get(self.url)
+        self.click(self.get_list_resource)
+        self.get_json(self.result_field, json_single_resource)
+        self.get_text(self.status_code, status_codes["200"])
 
+    def test_get_list_resource_not_found(self):
+        self.driver.get(self.url)
+        self.click(self.test_get_list_resource_not_found)
+        self.get_json(self.result_field, json_single_resource_not_found)
+        self.get_text(self.status_code, status_codes["404"])
+
+    def test_get_json_create(self):
+        self.driver.get(self.url)
+        self.click(self.post_create)
+        self.get_json(self.result_field, json_create)
+        self.get_text(self.status_code, status_codes["201"])
+
+    def test_get_json_put_update(self):
+        self.driver.get(self.url)
+        self.click(self.put_update)
+        self.get_json(self.result_field, json_update)
+        self.get_text(self.status_code, status_codes["200"])
+
+    def test_get_json_patch_update(self):
+        self.driver.get(self.url)
+        self.click(self.put_update)
+        self.get_json(self.result_field, json_update)
+        self.get_text(self.status_code, status_codes["200"])
+
+    def test_get_json_delete(self):
+        self.driver.get(self.url)
+        self.click(self.delete)
+        self.get_text(self.status_code, status_codes["200"])
+
+    def test_get_status_code_register(self):
+        self.driver.get(self.url)
+        self.click(self.post_register_successful)
+        self.get_json(self.result_field, json_register_successful)
+        self.get_text(self.status_code, status_codes["200"])
+
+    def test_get_status_code_register_unsuccssful(self):
+        self.driver.get(self.url)
+        self.click(self.post_register_unsuccessful)
+        self.get_json(self.result_field, json_register_unsuccessful)
+        self.get_text(self.status_code, status_codes["200"])
+
+    def test_get_status_code_login(self):
+        self.driver.get(self.url)
+        self.click(self.post_login_successful)
+        self.get_json(self.result_field, json_login)
+        self.get_text(self.status_code, status_codes["200"])
+
+    def test_get_status_code_login_unsuccessful(self):
+        self.driver.get(self.url)
+        self.click(self.post_login_unsuccessful)
+        self.get_json(self.result_field, json_login_unsuccessful)
+        self.get_text(self.status_code, status_codes["400"])
+
+    def test_get_status_code_delay_responce(self):
+        self.driver.get(self.url)
+        self.click(self.get_delayed_responce)
+        self.get_json(self.result_field, json_delay_responce)
+        self.get_text(self.status_code, status_codes["200"])
+
+    def test_check_json_in_other_tab(self):
+        self.driver.get(self.url)
+        self.click(self.path)
+        self.change_tab(-1)
+        self.get_json(self.other_page_with_json, json_get_list_user)
--- tests/test_get_user_list.py
+++ tests/test_get_user_list.py
@@ -1,20 +1,8 @@
-import time
-import pytest
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from pajes.main_page import TestMainPage
 
 
-def test_get_user_list():
-    options = webdriver.ChromeOptions()
-    options.add_argument('--ignore-certificate-errors')
-    options.add_argument('--ignore-ssl-errors')
-    options.add_experimental_option("detach", True)
-    options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    g = Service()
-    driver = webdriver.Chrome(options=options, service=g)
+def test_get_user_list(setup_chrome_driver):
+    driver = setup_chrome_driver
+    get_list_user = TestMainPage(driver)
+    get_list_user.test_get_list_user()
 
-    get_user_list = TestMainPage(driver)
-    get_user_list.test_get_list_user()
-
-    driver.quit()
